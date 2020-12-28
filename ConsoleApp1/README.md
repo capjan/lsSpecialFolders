3d26f911b6f7828ed9bd6c2232ca7238a03dfb60
Jan Ruhländer <jan.ruhlaender@gmail.com>
2020-12-28 12:03:50 +0100

    Initial Commit

---
+++ ConsoleApp1/README.md
@@ -0,0 +1,8 @@
+# lsSpecialFolders
+
+* This command line tool prints the .NET Special Folders to stdout.
+* It is just a debug helper tool that makes it easy for me to show the returned information on each platfrom.
+* It's based on my core library CoreLib.
+* MIT License.
+* That's it, nothing more to say.
+
---
+++ ConsoleApp1/lsSpecialFolders/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Core.Diagnostics.Impl;
+using Core.Enums;
+using Core.Environment;
+using Core.Environment.OperatingSystemInfoImpl;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            var osInfo = new OperatingSystemInfo();
+
+            
+            Console.WriteLine($"OS: {osInfo}");
+            
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            Console.WriteLine(".NET Special Folders");
+            Console.WriteLine(" Got via: Environment.GetFolderPath(SpecialFolder)");
+            Console.WriteLine("------------------------");
+            
+            var enumValues = EnumUtil.List<Environment.SpecialFolder>()
+                .OrderBy(i => i.Name)
+                .ToArray();
+            
+            var maxLength = enumValues.Max(i=>i.Name.Length);
+            
+            foreach (var itm in enumValues)
+            {
+                Console.Write($"{itm.Name}".PadLeft(maxLength));
+                Console.Write(" = ");
+                Console.WriteLine(Environment.GetFolderPath(itm.Value));    
+            }
+            
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Path.GetTempPath()");
+            Console.WriteLine("------------------------");
+
+            var tempPath = Path.GetTempPath();
+            Console.WriteLine($"TempPath = {tempPath}");
+            
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Path.GetTempFileName()");
+            Console.WriteLine("------------------------");
+            
+            var tempFileName = Path.GetTempFileName();
+            Console.WriteLine($"TempFileName = {tempFileName}");
+        }
+    }
+}
